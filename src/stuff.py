1e91aeec9028d45e0bb2804158643220860bb9e7
corentinbouton <corentin.btn@gmail.com>
2024-05-03 15:24:50 +0100

    cleaned duplicated notebooks

--- src/stuff.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-ndvi_val = pd.read_csv('./data/ndvi_val.csv')['ndvi'].to_list()
-
-validation_data = pd.read_csv('./data/validation_data.csv')
-
-validation_data.insert(4, 'ndvi', ndvi_val)
-
-validation_data.to_csv('./data/validation_data_ndvi.csv', index=False)
